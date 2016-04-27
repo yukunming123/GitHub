696194f4ae92308ac92fbdabb3101baa40ea94e7
yukunming123 <ms@yangguangyihao.local>
2016-04-27 16:25:56 +0800

    first
    这是我第一次玩Git

---
+++ GitHub/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHub
+//
+//  Created by 余昆明 on 16-4-27.
+//  Copyright (c) 2016年 余昆明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
